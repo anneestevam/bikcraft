25e91572d8b9d9b255a700bd7217fc58169fc608
Anne <helivianne@gmail.com>
2026-09-11 10:13:08 -0300

    galeria imagens

--- js/script.js
+++ js/script.js
@@ -45,3 +45,22 @@ function ativarPergunta(event) {
 
 
 perguntas.forEach(eventosPerguntas);
+
+
+// Galeria de bicicleta
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if(media) {
+        galeriaContainer.prepend(img)    
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
